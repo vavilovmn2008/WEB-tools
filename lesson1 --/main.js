68a84b855d8754343c04bde7004026f7d2f4f870
vavilovmn2008 <vavilovmn@yandex.ru>
2021-08-26 18:10:14 +0300

    Дз1-

---
+++ lesson1 --/main.js
@@ -0,0 +1,24 @@
+import { writeOutput, writeDiffOutput } from './output.js';
+import getDateDiff from './calcDateDiff.js';
+import {viewDiv} from './calcButton.js';
+
+
+const form = document.getElementById('dateDiffForm')
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    const formData =new FormData(event.target)
+    const dateFrom = formData.get('dateFrom')
+    const dateTo = formData.get('dateTo')
+
+    if(!dateFrom || !dateTo) {
+        writeOutput ('Ошибка');
+        return
+    }
+    writeDiffOutput (getDateDiff(dateFrom, dateTo))
+   };
+
+console.log(viewDiv);
+  
+
